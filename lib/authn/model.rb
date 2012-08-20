91925c5f7b06adb734526d48744d56e6c117a6c9
krainboltgreene <me@kurtisrainboltgreene.name>
2012-08-20 09:30:28 -0500

    Refactoring and adding documentation.

--- lib/authn/model.rb
+++ lib/authn/model.rb
@@ -1,30 +1,38 @@
 module AuthN
   module Model
-
-    def authenticate_using(provided_password)
-      password_digest == BCrypt::Password.new(provided_password)
-    end
-    private :authenticate_using
-
     def self.included(object)
       object.extend ClassMethods
     end
 
     module ClassMethods
       def self.has_authentication(o = {})
+        # Merge the provided options with defaults, if any options provided
         options = o.empty? ? AuthN::DEFAULTS.merge(o) : AuthN::DEFAULTS
+
+        # Define the authenticate method on the class
         define_authenticate_method
       end
 
-      # Return instance if account was found and password matched
-      # Return false if account was found and password didn't match
-      # Return nil if account wasn't found OR password wasn't given
       def self.authenticate(identifers = {}, password)
+        # Find the documents that match the criteria
         criteria = where identifiers
+
+        # Get the first document that matches the criteria
         instance = critera.first
-        if account && password
-          instance.authenticate password ? account : false
+
+        # Check to see if the instance exists and if a password was given
+        if instance && password
+          # Check to see if the instance can authenticate with password
+          if instance.authenticate password
+            instance
+          else
+            false
+          end
         end
+
+        # Return instance if account was found and password matched
+        # Return false if account was found and password didn't match
+        # Return nil if account wasn't found OR password wasn't given
       end
     end
   end
