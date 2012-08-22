d769f0b32947b2cb3b570cdea7190c068df8a21c
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-08-22 17:01:52 -0500

    Adding some empty tests for model logic

---
+++ test/lib/authn/model_test.rb
@@ -0,0 +1,68 @@
+require 'minitest/autorun'
+require_relative '../../helper'
+
+class TestAuthNModel < MiniTest::Unit::TestCase
+  def setup
+
+  end
+
+  # def has_authentication(o = {})
+  #   options = AuthN::Config::DEFAULTS.dup
+  #   options.merge! AuthN.config if AuthN.config.to_hash
+  #   options.merge! o unless o.empty?
+  #   config options
+  # end
+  def test_
+    actual =
+    expected =
+    assert_equal expected, actual
+  end
+
+  def test_
+    actual =
+    expected =
+    assert_equal expected, actual
+  end
+
+  def test_
+    actual =
+    expected =
+    assert_equal expected, actual
+  end
+
+  def test_
+    actual =
+    expected =
+    assert_equal expected, actual
+  end
+
+  # def authenticate(identifiers = {})
+  #   password = identifiers.delete :password
+  #   criteria = where identifiers
+  #   instance = criteria.first
+  #   if instance && password
+  #     if instance.authenticate password
+  #       instance
+  #     else
+  #       false
+  #     end
+  #   end
+  # end
+  def test_
+    actual =
+    expected =
+    assert_equal expected, actual
+  end
+
+  def test_
+    actual =
+    expected =
+    assert_equal expected, actual
+  end
+
+  def test_
+    actual =
+    expected =
+    assert_equal expected, actual
+  end
+end
