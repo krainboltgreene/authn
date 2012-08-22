8ce0d2aaf70cf2943c9b7a3c8768086707586180
krainboltgreene <me@kurtisrainboltgreene.name>
2012-08-22 15:38:49 -0500

    Switching to an easier to read version of the login and controller helpers

--- test/lib/authn/session_test.rb
+++ test/lib/authn/session_test.rb
@@ -5,32 +5,27 @@ class TestAuthNSession < MiniTest::Unit::TestCase
   def setup
     AuthN.config.account_klass = :Account
     Account.password "12341234"
-    @controller = Controller
+    Controller.instance_variable_set :@session, {}
   end
 
   # def login(identifiers, password, klass = AuthN.config.account_klass)
   #   generate_session_and_instance_from klass.authenticate identifiers, password
   # end
   def test_that_login_returns_account_given_good_credentials
-    actual = @controller.class_eval do
-      login email: "kurtis@example.com", password: "12341234"
-    end.class
+    instance = Controller.login email: "kurtis@example.com", password: "12341234"
+    actual = instance.class
     expected = Account
     assert_equal expected, actual
   end
 
   def test_that_login_returns_false_if_bad_password
-    actual = @controller.class_eval do
-      login email: "kurtis@example.com", password: "12341233"
-    end
+    actual = Controller.login email: "kurtis@example.com", password: "12341233"
     expected = false
     assert_equal expected, actual
   end
 
   def test_that_login_returns_nil_if_no_account_found
-    actual = @controller.class_eval do
-      login email: "kurti@example.com", password: "12341234"
-    end
+    actual = Controller.login email: "kurti@example.com", password: "12341234"
     expected = false
     assert_equal expected, actual
   end
