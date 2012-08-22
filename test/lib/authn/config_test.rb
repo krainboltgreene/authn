7f603ecc99edeb4f8f1799e4bd34c697ab0b0125
krainboltgreene <me@kurtisrainboltgreene.name>
2012-08-22 15:39:10 -0500

    Creaing a template test file for auth config

---
+++ test/lib/authn/config_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require_relative '../../helper'
+
+class TestAuthNConfig < MiniTest::Unit::TestCase
+  def setup
+
+  end
+
+  def test_
+    actual =
+    expected =
+    assert_equal expected, actual
+  end
+end
