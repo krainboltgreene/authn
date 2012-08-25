7ecabdb3e81a1bad2e8e508da88c283ece6d0f22
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-08-25 08:19:27 -0500

    Renaming the config options

--- lib/authn/config.rb
+++ lib/authn/config.rb
@@ -7,10 +7,10 @@ module AuthN
     include AltStruct::M
     DEFAULTS = {
       account_klass: :Account,
-      password_digest_name: :password_digest,
-      login_password_name: :password,
-      model_id_name: :id,
-      model_critera_name: :where
+      password_digest_method: :password_digest,
+      login_password_key: :password,
+      model_id_method: :id,
+      model_critera_method: :where,
     }
 
   end
