814189ca5e30275de762487dc148a5a53d87ccc3
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2012-09-05 13:34:05 -0500

    Updating model test documentation, writing some tests, renaming test methods

--- test/lib/authn/version_test.rb
+++ test/lib/authn/version_test.rb
@@ -3,6 +3,6 @@ require_relative '../../helper'
 
 class TestAuthNVersion < MiniTest::Unit::TestCase
   def test_that_version_is_latest
-    assert_equal "3.0.1", AuthN::VERSION
+    assert_equal "3.1.0", AuthN::VERSION
   end
 end
