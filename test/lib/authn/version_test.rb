95604af87b4db00dc83526ffdb7a3e9ff750dd90
krainboltgreene <me@kurtisrainboltgreene.name>
2012-09-08 23:48:51 -0500

    Removing useless version test

--- test/lib/authn/version_test.rb
+++
@@ -1,8 +0,0 @@
-require 'minitest/autorun'
-require 'helper'
-
-class TestAuthNVersion < MiniTest::Unit::TestCase
-  def test_that_version_is_latest
-    assert_equal "3.1.4", AuthN::VERSION
-  end
-end
